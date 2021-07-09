6f0edf04a6e58df5d1a143e903eba636fe590121
Szymon Perlicki <ikcilrep@outlook.com>
2021-07-09 21:57:58 +0200

    Add token's constructors and make text a field

--- src/tokenizer/tokens/token.cpp
+++ src/tokenizer/tokens/token.cpp
@@ -3,6 +3,15 @@
 
 using namespace spekter;
 
-std::string token::get_text() {
-    return this->text;
-}
+token::token(token_type type, int line_number,int char_in_line_number, int char_number) :
+    type(type),
+    line_number(line_number),
+    char_in_line_number(char_in_line_number),
+    char_number(char_number) {}
+
+token::token(token_type type, int line_number,int char_in_line_number, int char_number, std::string text) :
+    type(type),
+    line_number(line_number),
+    char_in_line_number(char_in_line_number),
+    char_number(char_number),
+    text(std::make_optional<std::string>(text)) {}
--- src/tokenizer/tokens/token.h
+++ src/tokenizer/tokens/token.h
@@ -3,17 +3,20 @@
 
 #include "token_type.h"
 #include <string>
+#include <optional>
 
 namespace spekter {
 
 class token {
-    std::string text;
     public:
         token_type type;
         int line_number;
         int char_in_line_number; 
         int char_number;
-        std::string get_text();
+        std::optional<std::string> text;
+        
+        token(token_type type, int line_number,int char_in_line_number, int char_number);
+        token(token_type type, int line_number,int char_in_line_number, int char_number, std::string text); 
 };
 }
 
