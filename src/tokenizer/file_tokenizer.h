73c6d0be62d4d974b6259e43241c2ccd1d38474d
Szymon Perlicki <ikcilrep@outlook.com>
2021-07-10 16:44:15 +0200

    Define file_tokenizer

---
+++ src/tokenizer/file_tokenizer.h
@@ -0,0 +1,16 @@
+#ifndef file_tokenizer_h
+#define file_tokenizer_h
+
+#include <string>
+#include "tokenizer.h"
+
+namespace spekter
+{
+    class file_tokenizer : tokenizer
+    {
+    public:
+        file_tokenizer(const std::string &code_path);
+    };
+}
+
+#endif
